a8d093ecb7387a48008b7b33c4a2986405ed71d3
Skizz1 <loic.destefano@epitech.eu>
2016-04-16 22:10:39 +0200

    push upload drag n drop

--- public/mywac.js
+++ public/mywac.js
@@ -1,12 +1,42 @@
-$(document).ready(function() {
-	var socket = io.connect('http://localhost:3333');
-
-	$('#inscription').click(function(e) {
+$(document).ready(function () {
+	socket = io.connect('http://localhost:3333');
+	$(".submit").click(function(e){
 		e.preventDefault();
-		var pseudo = $('#pseudo').val();
-		var email = $('#email').val();
-		var pass = $('#password').val();
+		var first_name = $("#first_name").val();
+		var email = $("#email").val();
+		var password = $("#password").val();
+		socket.emit("insciption",first_name,email,password);
+	})
+	
+	var selDiv = "";
 
-		socket.emit('login', pseudo, email, pass);
+	document.addEventListener("DOMContentLoaded", init, false);
+
+	function init() {
+		document.querySelector('#files').addEventListener('change', handleFileSelect, false);
+		selDiv = document.querySelector("#selectedFiles");
+	}
+
+	function handleFileSelect(e) {
+
+		if(!e.target.files) return;
+
+		selDiv.innerHTML = "";
+
+		var files = e.target.files;
+		console.log(files);
+		for(var i=0; i<files.length; i++) {
+			var f = files[i];
+
+			selDiv.innerHTML += f.name + "<br/>";
+
+		}
+
+	}
+
+	socket.on("insciption",function(wid)
+	{
+		console.log("s",wid)
+		$(".error").html("<p>Votre pseudonyme ou adresse email existe déjà</p>")
 	});
 });
--- serveur.js
+++ serveur.js
@@ -1,24 +1,37 @@
 var socket = require('socket.io');
-var mysql = require('mysql');
 var fs = require('fs');
+var mysql = require("mysql");
+var express = require('express');
+var path = require('path');
 
+var app = express();
 var con = mysql.createConnection({
-	host     : "localhost",
-	user     : "root",
-	password : "wac",
-	database : "wacdoc"
+	host: "localhost",
+	user: "root",
+	password: "wac",
+	database: "wacdoc"
+});
+var crypto = require('crypto');
+
+
+app.get('/login', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/login.html'));
 });
 
-// var toto = socket.on(pseudo, email, pass);
-// console.log(toto);
+app.get('/register', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/register.html'));
+});
 
-
-
-var express = require('express');
-var app = express();
+app.get('/upload', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/upload.html'));
+});
 
 // dans le dossier public a la racine
 app.use(express.static(__dirname + '/public'));
+
+//  con.query("SELECT * from users where username = 'root' ",function(err,rows){
+//   console.log(rows);
+// });
 
 
 var server = app.listen(3333, function ()
@@ -26,22 +39,37 @@ var server = app.listen(3333, function ()
 	var host = server.address().address
 	var port = server.address().port
 	console.log("Example app listening at http://%s:%s", host, port)
-})
-
-var io = require('socket.io').listen(server);
-
-io.sockets.on('connection', function (socket)
-{
-	socket.on('login', function (pseudo, email, pass) {
-		console.log(pseudo, email, pass);
-	});
-
-	con.query("SELECT * FROM users WHERE username = 'Skizz' ", function(err, rows) {
-		// console.log(rows);
-	});
-
-	// console.log('Un client est connecté !');
 });
 
 
-server.listen(3333);
+
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket)
+{
+	socket.on("insciption",function(first_name,email,password){
+		con.query("SELECT * from users where username='"+first_name+"' OR email ='" +email+"' ",function(err,rows){
+		  console.log(rows[0].username);
+		  console.log(rows[0].email);
+
+			if(rows[0].username == first_name || rows[0].email == email)
+			{
+				   socket.emit("insciption","error");  
+				   console.log(email);
+			}
+			else
+			{
+				var md5 = crypto.createHash('md5').update(password).digest("hex");
+				con.query("INSERT INTO users (username,email,password) VALUES ('" +first_name + "','" + email + "','"+md5 +"') ",function(err,rows){
+					console.log(err);
+
+				})
+			}
+		});
+				console.log('Un client est connecté !');
+	});
+})
+	server.listen(3333);
