9436d433c5ad40a150ae7c802cb2fae8bc2b9b1a
LabinotAvdiu <labinot.avdiu@epitech.eu>
2016-04-16 17:30:22 +0200

    register

--- public/jquery.js
+++ public/jquery.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
 	socket = io.connect('http://localhost:3333');
-
-
+	$(".submit").click(function(e){
+	e.preventDefault();
+		var first_name = 	$("#first_name").val();
+		var email = 	$("#email").val();
+		var password = 	$("#password").val();
+	socket.emit("insciption",first_name,email,password);
+	})
 	// socket.emit("pencil",X1,Y1,X-2,Y-5,null, $("#color").val(),range());
 
 
-	// socket.on("verihoszstylo",function(simetrie_poinx,simetrie_poinx2, simetrie_poiny,simetrie_poiny2,color,wid)
-	// {
-
-	// });
+	socket.on("insciption",function(wid)
+	{
+		console.log("s",wid)
+		$(".error").html("<p>Votre username ou email exist deja</p>")
+	});
 });
 
--- severNod.js
+++ severNod.js
@@ -1,18 +1,40 @@
 var socket = require('socket.io');
 var fs = require('fs');
-
+var mysql = require("mysql");
 var express = require('express');
+var path = require('path');
+
 var app = express();
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "root",
+	database: "wacdoc"
+});
+var crypto = require('crypto');
+
+
+app.get('/login', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/login.html'));
+});
+
+app.get('/register', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/register.html'));
+});
 
 // dans le dossier public a la racine
 app.use(express.static(__dirname + '/public'));
 
+//  con.query("SELECT * from users where username = 'root' ",function(err,rows){
+//   console.log(rows);
+// });
+
 
 var server = app.listen(3333, function ()
 {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://%s:%s", host, port)
+	var host = server.address().address
+	var port = server.address().port
+	console.log("Example app listening at http://%s:%s", host, port)
 })
 
 
@@ -24,9 +46,26 @@ var io = require('socket.io').listen(server);
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket)
 {
+	socket.on("insciption",function(first_name,email,password){
+		con.query("SELECT * from users where username='"+first_name+"' OR email ='" +email+"' ",function(err,rows){
+		  console.log(rows[0].username);
+		  console.log(rows[0].email);
 
+			if(rows[0].username == first_name || rows[0].email == email)
+			{
+				   socket.emit("insciption","error");  
+				   console.log(email);
+			}
+			else
+			{
+				var md5 = crypto.createHash('md5').update(password).digest("hex");
+				con.query("INSERT INTO users (username,email,password) VALUES ('" +first_name + "','" + email + "','"+md5 +"') ",function(err,rows){
+					console.log(err);
 
-  console.log('Un client est connecté !');
-});
-
-server.listen(3333);
+				})
+			}
+		});
+				console.log('Un client est connecté !');
+	});
+})
+	server.listen(3333);
